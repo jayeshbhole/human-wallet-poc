95c04200eefb4f2b5428b1d40aca47970c625121
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2023-03-26 14:12:40 +0530

    feat: add hook to handle 6 digit inputs

---
+++ src/hooks/useDigitInputs.ts
@@ -0,0 +1,135 @@
+import { createRef, Dispatch, SetStateAction, useContext, useMemo, useState } from 'react';
+
+const RE_DIGIT = new RegExp(/^\d+$/);
+
+const useDigitInputs = ({ value, setValue }: { value: string; setValue: Dispatch<SetStateAction<string>> }) => {
+  const valueItems = useMemo(() => {
+    const valueArray = value.split('');
+    const items: Array<string> = [];
+
+    for (let i = 0; i < 6; i++) {
+      const char = valueArray[i];
+
+      if (RE_DIGIT.test(char)) {
+        items.push(char);
+      } else {
+        items.push('');
+      }
+    }
+
+    return items;
+  }, [value]);
+
+  const targetRefs = useMemo(() => {
+    return Array.from({ length: 6 }).map(() => createRef<HTMLInputElement>());
+  }, []);
+
+  const inputOnChange = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
+    const target = e.target;
+    let targetValue = target.value.trim();
+    const isTargetValueDigit = RE_DIGIT.test(targetValue);
+
+    if (!isTargetValueDigit && targetValue !== '') {
+      return;
+    }
+
+    targetValue = isTargetValueDigit ? targetValue : ' ';
+
+    const targetValueLength = targetValue.length;
+
+    if (targetValueLength === 1) {
+      const newValue = value.substring(0, idx) + targetValue + value.substring(idx + 1);
+
+      setValue(newValue);
+
+      if (!isTargetValueDigit) {
+        return;
+      }
+
+      focusToNextInput(target);
+    } else if (targetValueLength === 6) {
+      setValue(targetValue);
+
+      target.blur();
+    }
+  };
+  const focusToNextInput = (target: HTMLElement) => {
+    const nextElementSibling = target.nextElementSibling as HTMLInputElement | null;
+
+    if (nextElementSibling) {
+      nextElementSibling.focus();
+    }
+  };
+  const focusToPrevInput = (target: HTMLElement) => {
+    const previousElementSibling = target.previousElementSibling as HTMLInputElement | null;
+
+    if (previousElementSibling) {
+      previousElementSibling.focus();
+    }
+  };
+  const inputOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    const key = e.key;
+    if (key === 'ArrowRight' || key === 'ArrowDown') {
+      e.preventDefault();
+      return focusToNextInput(target);
+    }
+
+    if (key === 'ArrowLeft' || key === 'ArrowUp') {
+      e.preventDefault();
+      return focusToPrevInput(target);
+    }
+
+    const targetValue = target.value;
+
+    // keep the selection range position
+    // if the same digit was typed
+    target.setSelectionRange(0, targetValue.length);
+
+    if (e.key !== 'Backspace' || target.value !== '') {
+      return;
+    }
+
+    // is a backspace input
+    if (targetValue === '') {
+      focusToPrevInput(target);
+    } else target.value = '';
+  };
+  const inputOnFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { target } = e;
+    const prevInputEl = target.previousElementSibling as HTMLInputElement | null;
+
+    if (prevInputEl && prevInputEl.value === '') {
+      return prevInputEl.focus();
+    }
+
+    target.setSelectionRange(0, target.value.length);
+  };
+
+  const handleKeyPadClick = (value: string) => {
+    const lastInputIdx = valueItems.findIndex((item) => item === '') - 1;
+
+    if (value === 'backspace') {
+      setValue((prev) => prev.slice(0, -1).slice(0, 6));
+      const target = targetRefs?.[lastInputIdx]?.current;
+      target?.focus();
+    } else {
+      const target = targetRefs?.[lastInputIdx + 1]?.current;
+      if (!target) return;
+
+      target.value = value;
+      inputOnChange({ target, currentTarget: target } as any, lastInputIdx + 1);
+    }
+  };
+
+  return {
+    valueItems,
+    targetRefs,
+    inputOnChange,
+    inputOnKeyDown,
+    inputOnFocus,
+    handleKeyPadClick,
+  };
+};
+
+export default useDigitInputs;
--- src/pages/Onboarding/SelectPin.tsx
+++ src/pages/Onboarding/SelectPin.tsx
@@ -1,5 +1,108 @@
-const SelectPin = () => {
-  return <div>index</div>;
+import { Box, Flex, Icon, Input } from '@chakra-ui/react';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
+import { useContext, useState } from 'react';
+import Keypad from '../../components/Keypad';
+import { OnboardingActionButton } from '../../components/Onboarding';
+import { HeadingBox, HeadingEmphasis, StepDescription, StepTitle } from '../../components/Onboarding/headings';
+import { OnboardingContext } from '../../contexts/OnboardingContext';
+import useDigitInputs from '../../hooks/useDigitInputs';
+
+const RE_DIGIT = new RegExp(/^\d+$/);
+
+const SelectDevicePin = () => {
+  const [pinValue, setPinValue] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const { selectUsernameAndDeploy } = useContext(OnboardingContext);
+
+  const handleAction = () => {
+    selectUsernameAndDeploy(pinValue);
+  };
+
+  const { valueItems, targetRefs, inputOnChange, inputOnKeyDown, inputOnFocus, handleKeyPadClick } = useDigitInputs({
+    value: pinValue,
+    setValue: setPinValue,
+  });
+
+  return (
+    <>
+      <HeadingBox>
+        <StepTitle>
+          <>
+            Create a <br />
+            <HeadingEmphasis>Device PIN</HeadingEmphasis>
+          </>
+        </StepTitle>
+        <StepDescription>Create a PIN to secure your wallet on this device.</StepDescription>
+      </HeadingBox>
+
+      <Flex direction="column">
+        <Box
+          display="grid"
+          gridTemplateColumns="repeat(7, 1fr)"
+          gridGap={4}>
+          {valueItems.map((digit, idx) => (
+            <Input
+              key={idx}
+              type={passwordVisible ? 'text' : 'password'}
+              inputMode="numeric"
+              pattern="\d{1}"
+              maxLength={6}
+              value={digit}
+              onChange={(e) => inputOnChange(e, idx)}
+              onKeyDown={inputOnKeyDown}
+              onFocus={inputOnFocus}
+              ref={targetRefs[idx]}
+              // chakra ui
+              variant="unstyled"
+              fontSize={'2xl'}
+              fontWeight={500}
+              color="blackAlpha.700"
+              textAlign="center"
+              borderBottomWidth={2}
+              borderBottomColor="blackAlpha.400"
+              rounded="none"
+              _focus={{
+                borderBottomColor: 'blackAlpha.800',
+                outline: 'none',
+              }}
+              _focusVisible={{
+                outline: 'none',
+              }}
+              _placeholder={{
+                fontWeight: 500,
+              }}
+              _hover={{
+                cursor: 'text',
+              }}
+              _disabled={{
+                cursor: 'not-allowed',
+                opacity: 0.5,
+              }}
+            />
+          ))}
+
+          {/* see password toggle */}
+          <Icon
+            alignSelf="center"
+            aria-label="toggle password visibility"
+            h={6}
+            w={6}
+            color="blackAlpha.400"
+            fontSize="md"
+            cursor="pointer"
+            onClick={() => setPasswordVisible((prev) => !prev)}
+            as={!passwordVisible ? EyeIcon : EyeSlashIcon}
+          />
+        </Box>
+      </Flex>
+
+      {/* keypad */}
+      <Keypad onKeypadClick={handleKeyPadClick} />
+
+      {/* Action Button */}
+      <OnboardingActionButton onClick={handleAction}>Select Username</OnboardingActionButton>
+    </>
+  );
 };
 
-export default SelectPin;
+export default SelectDevicePin;
