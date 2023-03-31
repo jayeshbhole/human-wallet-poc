b900d53f0cf694755ae74db5089b33ef5e5d0243
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2023-04-01 01:08:58 +0530

    feat: FinishOnboarding page contents

--- src/pages/Onboarding/FinishOnboarding/index.tsx
+++ src/pages/Onboarding/FinishOnboarding/index.tsx
@@ -1,5 +1,71 @@
+import { Box } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { OnboardingActionButton } from '../../../components/Onboarding';
+import { HeadingBox, HeadingEmphasis, StepDescription, StepTitle } from '../../../components/Onboarding/headings';
+import { useKeyringContext } from '../../../contexts/KeyringContext';
+
 const FinishOnboarding = () => {
-  return <div>index</div>;
+  const { activeAccount } = useKeyringContext();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeadingBox>
+        <StepTitle>
+          <>
+            Congratulations!
+            <br /> You've <HeadingEmphasis>created a new account</HeadingEmphasis>
+          </>
+        </StepTitle>
+        <StepDescription>Share your account address and username to start receiving payments!</StepDescription>
+      </HeadingBox>
+
+      <Box
+        key={activeAccount?.username}
+        p={4}
+        border="2px solid"
+        borderColor="gray.600"
+        borderRadius="2xl"
+        mb={4}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        position="relative"
+        _before={{
+          content: '""',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          width: 'calc(100% - 8px)',
+          height: 'calc(100% - 8px)',
+          background: 'gray.100',
+          borderRadius: 'xl',
+          transform: 'translate(-50%, -50%)',
+        }}>
+        <Box
+          zIndex="1"
+          mb="2"
+          fontSize="xl"
+          fontWeight="600"
+          color="text.primary">
+          {activeAccount?.username}
+        </Box>
+        <Box
+          zIndex="1"
+          fontSize="sm"
+          fontWeight="500"
+          color="gray.400"
+          w="15ch"
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis">
+          {activeAccount?.accountAddress}
+        </Box>
+      </Box>
+
+      <OnboardingActionButton onClick={() => navigate('/wallet')}>Open Wallet</OnboardingActionButton>
+    </>
+  );
 };
 
 export default FinishOnboarding;
